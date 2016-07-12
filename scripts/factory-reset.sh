8cfced993da70dc628f437f9eac4b26d92ae6c8a
Jason Borowski <esc0rtd3w@gmail.com>
2016-07-12 07:03:35 -0400

    Added Factory Reset Shell Script

---
+++ scripts/factory-reset.sh
@@ -0,0 +1,45 @@
+#!/system/bin/sh
+
+clear
+echo "Factory Resetting...."
+echo ""
+echo ""
+
+
+#mount -o rw,remount /data
+
+
+# Preserve ADB Settings
+cp /data/data/com.amazon.tv.settings/shared_prefs/com.amazon.tv.settings_preferences.xml /sdcard/
+
+
+#<?xml version='1.0' encoding='utf-8' standalone='yes' ?>
+#<map>
+#    <string name="countryOfResidence">US</string>
+#    <boolean name="whisperplay_settings_key" value="false" />
+#    <boolean name="enable_unknown_sources_key" value="true" />
+#    <boolean name="enable_adb_key" value="true" />
+#    <boolean name="notifications_settings_key" value="false" />
+#    <boolean name="report_usage" value="false" />
+#    <string name="preferredMarketplace">ATVPDKIKX0DER</string>
+#</map>
+
+# Removing /data/
+rm -rf /data/data/
+
+# Rebuilding empty /data/
+mkdir /data/data/
+
+# Restoring ADB Settings
+mkdir /data/data/com.amazon.tv.settings
+mkdir /data/data/com.amazon.tv.settings/shared_prefs
+cp /sdcard/com.amazon.tv.settings_preferences.xml /data/data/com.amazon.tv.settings/shared_prefs/com.amazon.tv.settings_preferences.xml
+
+# Clearing Dalvik Cache
+rm -rf /data/dalvik-cache
+rm -rf /cache/dalvik-cache
+
+
+
+reboot
+
