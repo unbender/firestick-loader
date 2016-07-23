527d61c5bfbf089f8e52e589b5ed1a74d31c7205
Jason Borowski <esc0rtd3w@gmail.com>
2016-07-23 07:04:37 -0400

    Fixed Recurse

--- scripts/kodi/android/clean-kodi-sports.cmd
+++ scripts/kodi/android/clean-kodi-sports.cmd
@@ -27,39 +27,39 @@ echo.
 echo.
 echo.
 
-%shell% rm %pathAndroid%/addons/plugin.video.ChampionSports
-%shell% rm %pathAndroid%/addons/plugin.video.DCSports
-%shell% rm %pathAndroid%/addons/plugin.video.MoneySports
-%shell% rm %pathAndroid%/addons/plugin.video.NBA
-%shell% rm %pathAndroid%/addons/plugin.video.NHLVideo
-%shell% rm %pathAndroid%/addons/plugin.video.NJMSoccer
-%shell% rm %pathAndroid%/addons/plugin.video.SportsDevil
-%shell% rm %pathAndroid%/addons/plugin.video.UFC
-%shell% rm %pathAndroid%/addons/plugin.video.adriansports
-%shell% rm %pathAndroid%/addons/plugin.video.all_wrestling
-%shell% rm %pathAndroid%/addons/plugin.video.espn_3
-%shell% rm %pathAndroid%/addons/plugin.video.extreme.com
-%shell% rm %pathAndroid%/addons/plugin.video.fcs
-%shell% rm %pathAndroid%/addons/plugin.video.motorstv.com
-%shell% rm %pathAndroid%/addons/plugin.video.nbaondemand
-%shell% rm %pathAndroid%/addons/plugin.video.nbcsn
-%shell% rm %pathAndroid%/addons/plugin.video.nbcsnliveextra
-%shell% rm %pathAndroid%/addons/plugin.video.nfl-teams
-%shell% rm %pathAndroid%/addons/plugin.video.nfl.com
-%shell% rm %pathAndroid%/addons/plugin.video.nhlondemand
-%shell% rm %pathAndroid%/addons/plugin.video.nhlstreams
-%shell% rm %pathAndroid%/addons/plugin.video.p2psport
-%shell% rm %pathAndroid%/addons/plugin.video.prosport
-%shell% rm %pathAndroid%/addons/plugin.video.redbull.tv
-%shell% rm %pathAndroid%/addons/plugin.video.si
-%shell% rm %pathAndroid%/addons/plugin.video.speedway
-%shell% rm %pathAndroid%/addons/plugin.video.sportcenterhd
-%shell% rm %pathAndroid%/addons/plugin.video.sportsmix
-%shell% rm %pathAndroid%/addons/plugin.video.spoxtv
-%shell% rm %pathAndroid%/addons/plugin.video.superreplays
-%shell% rm %pathAndroid%/addons/plugin.video.youtube.ufc
-%shell% rm %pathAndroid%/addons/plugin.video.youtube.wwe
-%shell% rm %pathAndroid%/addons/repository.adriansports
+%shell% rm -r %pathAndroid%/addons/plugin.video.ChampionSports
+%shell% rm -r %pathAndroid%/addons/plugin.video.DCSports
+%shell% rm -r %pathAndroid%/addons/plugin.video.MoneySports
+%shell% rm -r %pathAndroid%/addons/plugin.video.NBA
+%shell% rm -r %pathAndroid%/addons/plugin.video.NHLVideo
+%shell% rm -r %pathAndroid%/addons/plugin.video.NJMSoccer
+%shell% rm -r %pathAndroid%/addons/plugin.video.SportsDevil
+%shell% rm -r %pathAndroid%/addons/plugin.video.UFC
+%shell% rm -r %pathAndroid%/addons/plugin.video.adriansports
+%shell% rm -r %pathAndroid%/addons/plugin.video.all_wrestling
+%shell% rm -r %pathAndroid%/addons/plugin.video.espn_3
+%shell% rm -r %pathAndroid%/addons/plugin.video.extreme.com
+%shell% rm -r %pathAndroid%/addons/plugin.video.fcs
+%shell% rm -r %pathAndroid%/addons/plugin.video.motorstv.com
+%shell% rm -r %pathAndroid%/addons/plugin.video.nbaondemand
+%shell% rm -r %pathAndroid%/addons/plugin.video.nbcsn
+%shell% rm -r %pathAndroid%/addons/plugin.video.nbcsnliveextra
+%shell% rm -r %pathAndroid%/addons/plugin.video.nfl-teams
+%shell% rm -r %pathAndroid%/addons/plugin.video.nfl.com
+%shell% rm -r %pathAndroid%/addons/plugin.video.nhlondemand
+%shell% rm -r %pathAndroid%/addons/plugin.video.nhlstreams
+%shell% rm -r %pathAndroid%/addons/plugin.video.p2psport
+%shell% rm -r %pathAndroid%/addons/plugin.video.prosport
+%shell% rm -r %pathAndroid%/addons/plugin.video.redbull.tv
+%shell% rm -r %pathAndroid%/addons/plugin.video.si
+%shell% rm -r %pathAndroid%/addons/plugin.video.speedway
+%shell% rm -r %pathAndroid%/addons/plugin.video.sportcenterhd
+%shell% rm -r %pathAndroid%/addons/plugin.video.sportsmix
+%shell% rm -r %pathAndroid%/addons/plugin.video.spoxtv
+%shell% rm -r %pathAndroid%/addons/plugin.video.superreplays
+%shell% rm -r %pathAndroid%/addons/plugin.video.youtube.ufc
+%shell% rm -r %pathAndroid%/addons/plugin.video.youtube.wwe
+%shell% rm -r %pathAndroid%/addons/repository.adriansports
 
 
 echo.
@@ -70,39 +70,39 @@ echo.
 echo.
 echo.
 
-%shell% rm %pathAndroid%/userdata/addon_data/plugin.video.ChampionSports
-%shell% rm %pathAndroid%/userdata/addon_data/plugin.video.DCSports
-%shell% rm %pathAndroid%/userdata/addon_data/plugin.video.MoneySports
-%shell% rm %pathAndroid%/userdata/addon_data/plugin.video.NBA
-%shell% rm %pathAndroid%/userdata/addon_data/plugin.video.NHLVideo
-%shell% rm %pathAndroid%/userdata/addon_data/plugin.video.NJMSoccer
-%shell% rm %pathAndroid%/userdata/addon_data/plugin.video.SportsDevil
-%shell% rm %pathAndroid%/userdata/addon_data/plugin.video.UFC
-%shell% rm %pathAndroid%/userdata/addon_data/plugin.video.adriansports
-%shell% rm %pathAndroid%/userdata/addon_data/plugin.video.all_wrestling
-%shell% rm %pathAndroid%/userdata/addon_data/plugin.video.espn_3
-%shell% rm %pathAndroid%/userdata/addon_data/plugin.video.extreme.com
-%shell% rm %pathAndroid%/userdata/addon_data/plugin.video.fcs
-%shell% rm %pathAndroid%/userdata/addon_data/plugin.video.motorstv.com
-%shell% rm %pathAndroid%/userdata/addon_data/plugin.video.nbaondemand
-%shell% rm %pathAndroid%/userdata/addon_data/plugin.video.nbcsn
-%shell% rm %pathAndroid%/userdata/addon_data/plugin.video.nbcsnliveextra
-%shell% rm %pathAndroid%/userdata/addon_data/plugin.video.nfl-teams
-%shell% rm %pathAndroid%/userdata/addon_data/plugin.video.nfl.com
-%shell% rm %pathAndroid%/userdata/addon_data/plugin.video.nhlondemand
-%shell% rm %pathAndroid%/userdata/addon_data/plugin.video.nhlstreams
-%shell% rm %pathAndroid%/userdata/addon_data/plugin.video.p2psport
-%shell% rm %pathAndroid%/userdata/addon_data/plugin.video.prosport
-%shell% rm %pathAndroid%/userdata/addon_data/plugin.video.redbull.tv
-%shell% rm %pathAndroid%/userdata/addon_data/plugin.video.si
-%shell% rm %pathAndroid%/userdata/addon_data/plugin.video.speedway
-%shell% rm %pathAndroid%/userdata/addon_data/plugin.video.sportcenterhd
-%shell% rm %pathAndroid%/userdata/addon_data/plugin.video.sportsmix
-%shell% rm %pathAndroid%/userdata/addon_data/plugin.video.spoxtv
-%shell% rm %pathAndroid%/userdata/addon_data/plugin.video.superreplays
-%shell% rm %pathAndroid%/userdata/addon_data/plugin.video.youtube.ufc
-%shell% rm %pathAndroid%/userdata/addon_data/plugin.video.youtube.wwe
-%shell% rm %pathAndroid%/userdata/addon_data/repository.adriansports
+%shell% rm -r %pathAndroid%/userdata/addon_data/plugin.video.ChampionSports
+%shell% rm -r %pathAndroid%/userdata/addon_data/plugin.video.DCSports
+%shell% rm -r %pathAndroid%/userdata/addon_data/plugin.video.MoneySports
+%shell% rm -r %pathAndroid%/userdata/addon_data/plugin.video.NBA
+%shell% rm -r %pathAndroid%/userdata/addon_data/plugin.video.NHLVideo
+%shell% rm -r %pathAndroid%/userdata/addon_data/plugin.video.NJMSoccer
+%shell% rm -r %pathAndroid%/userdata/addon_data/plugin.video.SportsDevil
+%shell% rm -r %pathAndroid%/userdata/addon_data/plugin.video.UFC
+%shell% rm -r %pathAndroid%/userdata/addon_data/plugin.video.adriansports
+%shell% rm -r %pathAndroid%/userdata/addon_data/plugin.video.all_wrestling
+%shell% rm -r %pathAndroid%/userdata/addon_data/plugin.video.espn_3
+%shell% rm -r %pathAndroid%/userdata/addon_data/plugin.video.extreme.com
+%shell% rm -r %pathAndroid%/userdata/addon_data/plugin.video.fcs
+%shell% rm -r %pathAndroid%/userdata/addon_data/plugin.video.motorstv.com
+%shell% rm -r %pathAndroid%/userdata/addon_data/plugin.video.nbaondemand
+%shell% rm -r %pathAndroid%/userdata/addon_data/plugin.video.nbcsn
+%shell% rm -r %pathAndroid%/userdata/addon_data/plugin.video.nbcsnliveextra
+%shell% rm -r %pathAndroid%/userdata/addon_data/plugin.video.nfl-teams
+%shell% rm -r %pathAndroid%/userdata/addon_data/plugin.video.nfl.com
+%shell% rm -r %pathAndroid%/userdata/addon_data/plugin.video.nhlondemand
+%shell% rm -r %pathAndroid%/userdata/addon_data/plugin.video.nhlstreams
+%shell% rm -r %pathAndroid%/userdata/addon_data/plugin.video.p2psport
+%shell% rm -r %pathAndroid%/userdata/addon_data/plugin.video.prosport
+%shell% rm -r %pathAndroid%/userdata/addon_data/plugin.video.redbull.tv
+%shell% rm -r %pathAndroid%/userdata/addon_data/plugin.video.si
+%shell% rm -r %pathAndroid%/userdata/addon_data/plugin.video.speedway
+%shell% rm -r %pathAndroid%/userdata/addon_data/plugin.video.sportcenterhd
+%shell% rm -r %pathAndroid%/userdata/addon_data/plugin.video.sportsmix
+%shell% rm -r %pathAndroid%/userdata/addon_data/plugin.video.spoxtv
+%shell% rm -r %pathAndroid%/userdata/addon_data/plugin.video.superreplays
+%shell% rm -r %pathAndroid%/userdata/addon_data/plugin.video.youtube.ufc
+%shell% rm -r %pathAndroid%/userdata/addon_data/plugin.video.youtube.wwe
+%shell% rm -r %pathAndroid%/userdata/addon_data/repository.adriansports
 
 
 :end
