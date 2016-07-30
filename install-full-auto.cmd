14cace8c23f35c3ad377503f24823e83777ee34e
Jason Borowski <esc0rtd3w@gmail.com>
2016-07-30 04:58:22 -0400

    Removed Amazon Underground and Video From Scripting

--- install-full-auto.cmd
+++ install-full-auto.cmd
@@ -149,26 +149,26 @@ echo.
 %install% "apps\media\hulu.apk"
 
 
-cls
-%_color% 0e
-set appName=Amazon Underground
-set apk=
-echo.
-echo.
-echo Installing %appName%...
-echo.
-%install% "apps\media\amazon-underground.apk"
+::cls
+::%_color% 0e
+::set appName=Amazon Underground
+::set apk=
+::echo.
+::echo.
+::echo Installing %appName%...
+::echo.
+::%install% "apps\media\amazon-underground.apk"
 
 
-cls
-%_color% 0e
-set appName=Amazon Video
-set apk=
-echo.
-echo.
-echo Installing %appName%...
-echo.
-%install% "apps\media\amazon-video.apk"
+::cls
+::%_color% 0e
+::set appName=Amazon Video
+::set apk=
+::echo.
+::echo.
+::echo Installing %appName%...
+::echo.
+::%install% "apps\media\amazon-video.apk"
 
 
 cls
