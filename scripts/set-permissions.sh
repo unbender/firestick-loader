fc18735465d9e2daeaac315f46718348470811d9
Jason Borowski <esc0rtd3w@gmail.com>
2016-08-03 04:43:57 -0400

    Added /system/app/ To Permission Change

--- scripts/set-permissions.sh
+++ scripts/set-permissions.sh
@@ -6,6 +6,8 @@ echo ""
 echo ""
 
 
-find /system/priv-app -type d -exec chmod 0755 {} \;
+find /system/app -type d -exec chmod 0755 {} \;
+find /system/app -type f -exec chmod 0644 {} \;
 
+find /system/priv-app -type d -exec chmod 0755 {} \;
 find /system/priv-app -type f -exec chmod 0644 {} \;
