56671c322b4da083feadcbe2a614767175f60862
Jason Borowski <esc0rtd3w@gmail.com>
2016-08-09 07:22:24 -0400

    Updated Commenting

--- scripts/opera-as-system.sh
+++ scripts/opera-as-system.sh
@@ -10,4 +10,4 @@ cp /data/local/tmp/opera.apk /system/app/OperaMini/OperaMini.apk
 #chown 0.0 /system/priv-app/OperaMini/OperaMini.apk
 chmod 0644 /system/app/OperaMini/OperaMini.apk
 
-#am start -a de.belu.firestopper/.gui.MainActivity -n de.belu.firestopper/.gui.MainActivity
+#am start -a de.belu.firestopper/.gui.MainActivity -n 
