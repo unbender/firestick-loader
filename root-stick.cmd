cc49d56d09717ac40b86617d2835e279f467270a
Jason Borowski <esc0rtd3w@gmail.com>
2016-08-11 03:47:56 -0400

    Added sleep After Reload Root Option

--- root-stick.cmd
+++ root-stick.cmd
@@ -759,6 +759,8 @@ if %check%==s goto rootActive
 
 if %check%==R %shell% am start -a android.intent.action.MAIN -n com.kingroot.kinguser/.activitys.SliderMainActivity
 if %check%==r %shell% am start -a android.intent.action.MAIN -n com.kingroot.kinguser/.activitys.SliderMainActivity
+if %check%==R %sleep% 5
+if %check%==r %sleep% 5
 if %check%==R goto root2
 if %check%==r goto root2
 
